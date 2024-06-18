90d88e74d18a085a7d5b56c8f6105092f6ef8535
tetianamakar <divnatany17@gmail.com>
2024-06-18 12:48:14 +0300

    add repositories

--- src/main/java/guru/springframework/spring5webapp/domain/Author.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Author.java
@@ -1,4 +1,4 @@
-package domain;
+package guru.springframework.spring5webapp.domain;
 
 import lombok.Getter;
 import lombok.Setter;
--- src/main/java/guru/springframework/spring5webapp/domain/Book.java
+++ src/main/java/guru/springframework/spring5webapp/domain/Book.java
@@ -1,4 +1,4 @@
-package domain;
+package guru.springframework.spring5webapp.domain;
 
 import java.util.Objects;
 import java.util.Set;
